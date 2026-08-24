e3c82b085a8bacd10b87b20f0952913aed93b9ad
meacdev <matheuseduardoamazonas@gmail.com>
2026-08-24 15:15:49 -0400

    feat: adicionando rotas de administrador e customizador

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from 'react-router';
+
+//Loja
+
 import { Layout } from '../component/Layout';
 import { HomePage } from '../pages/HomePage';
 import { NotFoundPage } from '../pages/NotFoundPage';
@@ -6,6 +9,12 @@ import { CardapioPage } from '../features/pizzaria/pages/CardapioPage';
 import { CategoriaPage } from '../features/pizzaria/pages/CategoriaPage';
 import { CarrinhoPage } from '../features/pizzaria/pages/CarrinhoPage';
 import { PizzaDetalhePage } from '../features/pizzaria/pages/PizzaDetalhePage';
+
+// Admin
+
+import { LoginPage } from '../features/admin/pages/LoginPage';
+import { CustomizationPage } from '../features/admin/pages/CustomizationPage';
+import { ProtectedRoute } from '../features/admin/guards/ProtectedRoute';
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +27,20 @@ export const router = createBrowserRouter([
       { path: 'pizza/:slug', Component: PizzaDetalhePage },
       { path: 'carrinho', Component: CarrinhoPage },
       { path: '*', Component: NotFoundPage },
+    ],
+  },
+  {
+    path: 'admin',
+    children: [
+      { index: true, Component: LoginPage }, // /admin -> login
+      {
+        path: 'customizacao',
+        element: (
+          <ProtectedRoute>
+            <CustomizationPage />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ]);
